4d76a6eacc94e4a4f200f6aa4f75766c2d8b35e9
David Parks <dparks@seattletimes.com>
2018-07-31 11:32:41 -0700

    Adding back in dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mediapub_extensions",
-    version='0.0.1a2.dev3',
+    version='0.0.1a2.dev4',
     url='TBD',
     license="MIT",
     author="David Parks",
@@ -10,8 +10,8 @@ setup(
     description="Tools for Media Publishers",
     packages=find_packages(exclude=['tests']),
     # long_description=open('README.md').read(),
-    zip_safe=False
-    # install_requires=['snowflake-connector-python==1.6.3'],
+    zip_safe=False,
+    install_requires=['snowflake-connector-python==1.6.3']
     # setup_requires=[],
     # test_suite=''
 )
