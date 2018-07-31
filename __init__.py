230392d790f0e72c4da0dbd2495c8a474fcca340
David Parks <dparks@seattletimes.com>
2018-07-31 10:55:51 -0700

    More version testing

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from mediapub_extensions.ApiWrappers.Wordpress import Wordpress
 from mediapub_extensions.ApiWrappers.Snowflake import Snowflake
 
-__version__ = '0.0.1.PA'
+__version__ = '0.0.2.PA'
 __author__ = 'David Parks businessintelligence@seattletimes.com'
 __all__ = []
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mediapub_extensions",
-    version='0.0.1a1.dev2',
+    version='0.0.1a2.dev1',
     url='TBD',
     license="MIT",
     author="David Parks",
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     # long_description=open('README.md').read(),
     zip_safe=False,
-    install_requires=['snowflake-connector-python==1.6.3'],
+    # install_requires=['snowflake-connector-python==1.6.3'],
     # setup_requires=[],
     # test_suite=''
 )
