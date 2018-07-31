fd7b8feb10c4fd9fba787e3250b343ad32c5bfe8
David Parks <dparks@seattletimes.com>
2018-07-31 11:14:45 -0700

    more of the same

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mediapub_extensions.ApiWrappers.Wordpress import Wordpress
-from mediapub_extensions.ApiWrappers.Snowflake import Snowflake
+# from mediapub_extensions.ApiWrappers.Snowflake import Snowflake
 
 __version__ = '0.0.2.PA'
 __author__ = 'David Parks businessintelligence@seattletimes.com'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mediapub_extensions",
-    version='0.0.1a2.dev1',
+    version='0.0.1a2.dev2',
     url='TBD',
     license="MIT",
     author="David Parks",
