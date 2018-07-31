6297f25f5e4249eddbeedd4f270a8ee1687e81b7
David Parks <dparks@seattletimes.com>
2018-07-31 11:27:59 -0700

    Testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mediapub_extensions",
-    version='0.0.1a2.dev2',
+    version='0.0.1a2.dev3',
     url='TBD',
     license="MIT",
     author="David Parks",
@@ -10,7 +10,7 @@ setup(
     description="Tools for Media Publishers",
     packages=find_packages(exclude=['tests']),
     # long_description=open('README.md').read(),
-    zip_safe=False,
+    zip_safe=False
     # install_requires=['snowflake-connector-python==1.6.3'],
     # setup_requires=[],
     # test_suite=''
