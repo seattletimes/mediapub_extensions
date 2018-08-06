b8f60899a6b68dc181cb75ae66b5587981a16df7
David Parks <dparks@seattletimes.com>
2018-08-06 14:36:23 -0700

    Fixed looping issue

--- mediapub_extensions/ApiWrappers/Wordpress.py
+++ mediapub_extensions/ApiWrappers/Wordpress.py
@@ -128,17 +128,17 @@ class Wordpress(object):
             url = url + "page={}".format(page)
             iter = True
             if iter: #TODO remove this extra if.
-                # while page <= 10: # For testing, only get the first 10 pages instead of all of them. #TODO: remove
+                while 1 #page <= 10: # For testing, only get the first 10 pages instead of all of them. #TODO: remove
                     # Replace the page param with an updated one.
                     #NOTE: This fails to replace on the first iteration since there is no page -1 value found.  Expected.
-                url = url.replace('&page=' + str(page - 1), '&page=' + str(page))
-                # print(url)
-                try:
-                    response = requests.get(url, self.headers)
-                    items.extend(self.__handle_response(response))
-                except IndexError as e: # when the last page is reached stop iter
-                    break
-                page += 1
+                    url = url.replace('&page=' + str(page - 1), '&page=' + str(page))
+                    # print(url)
+                    try:
+                        response = requests.get(url, self.headers)
+                        items.extend(self.__handle_response(response))
+                    except IndexError as e: # when the last page is reached stop iter
+                        break
+                    page += 1
         return items
 
     def __build_posts_url(self, env='prod', post_id=None):
